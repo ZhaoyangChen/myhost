82d313b7f74270b9870cd40e51aab7375d16b6bf
陈赵阳 <zhaoyang@Royans-MacBook-Air.local>
2016-06-30 17:13:33 +0800

    尝试WordPress

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php	require_once ('lib/Template.php');?><!DOCTYPE html><html lang="en"><head>	<title>Zhaoyang</title>	<meta charset="utf-8">	<meta name="viewport" content="width=device-width, initial-scale=1">	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">	<link rel="stylesheet" href="css/nav.css">	<link rel="stylesheet" href="css/index.css">	<script src="js/jquery.min.js"></script>	<script src="bootstrap/js/bootstrap.min.js"></script></head><body>	<?php		$navTempate = new Template();		$navTempate->printNav();	?>	<div id="main">		<?php		$numOfImage = 5;		for($i=1; $i <=$numOfImage; $i++) {			echo "<div>					<img id=\"mainImage{$i}\" class=\"mainImage\" src=\"img/indexShow_{$i}.jpg\">				</div>";		}		?>	</div></body><script type="text/javascript">	var imageIndex  = 0;	var numOfImage = <?php echo $numOfImage?>;	$(document).ready(function(){		setInterval(imageLoop, 5000);	});	function imageLoop() {		imageIndex++;		if (imageIndex > numOfImage)			imageIndex = 1;		var nextIndex = imageIndex + 1;		if (nextIndex > numOfImage)			nextIndex = 1;		$('#mainImage' + imageIndex).fadeOut(2000);		$('#mainImage' + nextIndex).fadeIn(2000);	}</script></html>
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
