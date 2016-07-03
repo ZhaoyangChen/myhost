929be63205ffe2378718ab8e71670e51d15b0c9f
陈赵阳 <chenzhaoyang@baixing.com>
2016-07-03 12:01:09 +0800

    首页初步

--- application/views/home.php
+++ application/views/home.php
@@ -3,10 +3,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
+	<link rel="stylesheet" href="application/views/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="application/views/css/home.css" />
+
+	<script src="application/views/js/jquery-3.0.0.min.js"></script>
+	<script src="application/views/js/bootstrap.min.js"></script>
+
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>Royan's</title>
 </head>
+
 <body>
-<p>不破不立</p>
+	<nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="#">Royan's</a>
+		</div>
+		<div>
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="#">藏锋</a></li>
+				<li ><a href="#">聆夜</a></li>
+				<li ><a href="#">昭言</a></li>
+				<li ><a href="#">埋名</a></li>
+				<li ><a href="#">博主太懒,睡一会儿再来开工</a></li>
+			</ul>
+		</div>
+	</nav>
+
+	<div class="page-header">
+	</div>
+
+	<div>
+	</div>
 </body>
 </html>
