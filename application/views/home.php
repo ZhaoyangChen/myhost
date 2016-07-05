19c9a0f37f17b21ef5f94198856f93331d2260fb
陈赵阳 <chenzhaoyang@baixing.com>
2016-07-05 18:00:13 +0800

    首页躯干

--- application/views/home.php
+++ application/views/home.php
@@ -35,6 +35,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<div class="page-body">
 		<div class="portrait">
 		</div>
+		<div id="self-des" class="panel panel-default">
+			<div class="panel-body">
+				<h2>Royan Chen</h2>
+				<div class="col-lg-3">
+					<img class="circleTag" src="/assets/image/china.jpg">
+					<h2>天朝</h2>
+					<p>富强、民主、文明、和谐</p>
+					<p>自由、平等、公正、法治</p>
+
+				</div>
+				<div class="col-lg-3">
+					<img class="circleTag" src="/assets/image/zju2.jpg">
+					<h2>老和山职业技术学院</h2>
+					<p>诸位在校，有两个问题应该自己问问，第一，到浙大来做什么？第二，将来毕业后做什么样的人？</p>
+				</div>
+
+				<div class="col-lg-3">
+					<img class="circleTag" src="/assets/image/male.png">
+					<h2>直男癌</h2>
+					<p>我靠, 前面开车技术这么烂, 肯定是个女的!</p>
+
+				</div>
+				<div class="col-lg-3">
+					<img class="circleTag" src="/assets/image/operator.png">
+					<h2>搬砖工</h2>
+					<p>搬完这波攒够钱我就回老家结婚</p>
+				</div>
+
+			</div>
+		</div>
 	</div>
 </body>
 </html>
