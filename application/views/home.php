b4195a662e017ac5fef7a625a17911f1dec997dd
陈赵阳 <chenzhaoyang@baixing.com>
2016-09-09 16:31:20 +0800

    Loader

--- application/views/home.php
+++ application/views/home.php
@@ -17,6 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </head>
 
 <body>
+	<div class="se-pre-con"></div>
 	<nav class="navbar navbar-inverse navbar-static-top" role="navigation">
 		<div>
 			<ul class="nav navbar-nav">
